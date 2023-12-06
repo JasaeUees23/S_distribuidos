d867b7ad1db65714edce46feb68c3a301914c48f
JasaeUees23 <149432480+JasaeUees23@users.noreply.github.com>
2023-12-06 15:56:50 -0500

    Update api_sdistribuidos.py

--- api_sdistribuidos.py
+++ api_sdistribuidos.py
@@ -1,16 +1,1 @@
-from flask import Flask, request, jsonify
 
-app = Flask(__name__)
-
-@app.route('/distribution', methods=['POST'])
-def distribute_process():
-    task = request.json['distribution']
-
-    # Distribuir la tarea a los seguidores
-
-    m_results = []
-
-    return jsonify({'Resultados': m_results})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000)
